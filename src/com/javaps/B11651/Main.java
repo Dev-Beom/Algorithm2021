7b4cfdb9921c73b45fa8b0e3cfb926c7e0413d15
Dev-Beom <youngbeom0908@gmail.com>
2021-06-08 05:08:47 +0900

    [20210608] - 백준 11651

---
+++ src/com/javaps/B11651/Main.java
@@ -0,0 +1,47 @@
+package com.javaps.B11651;
+
+import java.io.*;
+import java.util.*;
+
+class Point {
+    final int x;
+    final int y;
+
+    Point(int x, int y) {
+        this.x = x;
+        this.y = y;
+    }
+}
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        int TC = Integer.parseInt(st.nextToken());
+
+        LinkedList<Point> points = new LinkedList<>();
+
+        for (int i = 0; i < TC; i++) {
+            st = new StringTokenizer(br.readLine());
+            int x = Integer.parseInt(st.nextToken());
+            int y = Integer.parseInt(st.nextToken());
+
+            points.add(new Point(x, y));
+        }
+
+        points.sort(((o1, o2) -> {
+            if (o1.y == o2.y) {
+                return o1.x - o2.x;
+            } else {
+                return o1.y - o2.y;
+            }
+        }));
+
+        points.forEach(
+                point -> {
+                    System.out.println(point.x + " " + point.y);
+                }
+        );
+    }
+}
