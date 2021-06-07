41a012f28ffcc2d165bdd9c4fd8d97e10de127c5
Dev-Beom <youngbeom0908@gmail.com>
2021-06-07 20:09:01 +0900

    [20210607] - 백준 15649 주석 추가

--- src/com/javaps/B15650/Main.java
+++ src/com/javaps/B15650/Main.java
@@ -1,4 +1,4 @@
-package com.javaps.B15649;
+package com.javaps.B15650;
 
 import java.util.*;
 import java.io.*;
@@ -24,8 +24,6 @@ public class Main {
     }
 
     public static void func(int depth) {
-
-        // 재귀 깊이가 M과 같아지면 탐색 과정에서 담았던 배열을 출력.
         if (depth == M) {
             for (int i = 0; i < M; i++) {
                 System.out.print(arr[i] + " ");
@@ -33,15 +31,11 @@ public class Main {
             System.out.println();
             return;
         }
-
         for (int i = 1; i <= N; i++) {
-            // 만약 해당 노드(값)을 방문하지 않았다면?
             if (!isVisited[i]) {
-                arr[depth] = i;         // 해당 노드를 방문 상태로 변경
-                isVisited[i] = true;    // 해당 깊이를 index 로 하여 i + 1 값 저장
-                func(depth + 1);    // 다음 자식 노드 방문을 위해 depth 1 증가시키면서 재귀 호출
-
-                // 자식 노드 방문이 끝나고 돌아오면 방문 노드를 방문하지 않은 상태로 변경.
+                arr[depth] = i;
+                isVisited[i] = true;
+                func(depth + 1);
                 isVisited[i] = false;
             }
         }
