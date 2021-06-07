18015727384b0de646e31ad2bc1d111aea08bc39
Dev-Beom <youngbeom0908@gmail.com>
2021-06-07 19:53:34 +0900

    [20210607] - 백준 15649

---
+++ src/com/javaps/B15649/Main.java
@@ -0,0 +1,39 @@
+package com.javaps.B15649;
+
+import java.io.*;
+import java.util.*;
+
+public class Main {
+    static int N;
+    static int M;
+    static int[] arr = new int[10];
+    static boolean[] isUsed = new boolean[10];
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        N = Integer.parseInt(st.nextToken());
+        M = Integer.parseInt(st.nextToken());
+
+        func(0);
+    }
+
+    public static void func(int k) {
+        if (k == M) {
+            for (int i = 0; i < M; i++) {
+                System.out.print(arr[i] + " ");
+            }
+            System.out.println();
+            return;
+        }
+        for (int i = 1; i <= N; i++) {
+            if (!isUsed[i]) {
+                arr[k] = i;
+                isUsed[i] = true;
+                func(k + 1);
+                isUsed[i] = false;
+            }
+        }
+    }
+}
