14b3f5045d4468c99b8fad13d6510ed9473ca4e1
Dev-Beom <youngbeom0908@gmail.com>
2021-06-07 20:19:11 +0900

    [20210607] - 백준 15650

--- src/com/javaps/B15650/Main.java
+++ src/com/javaps/B15650/Main.java
@@ -8,7 +8,6 @@ public class Main {
     static int M;
 
     static int[] arr;
-    static boolean[] isVisited;
 
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
@@ -17,27 +16,30 @@ public class Main {
         N = Integer.parseInt(st.nextToken());
         M = Integer.parseInt(st.nextToken());
 
-        arr = new int[10];
-        isVisited = new boolean[10];
+        arr = new int[M];
 
-        func(0);
+        dfs(1, 0);
     }
 
-    public static void func(int depth) {
+    public static void dfs(int at, int depth) {
         if (depth == M) {
+            // 깊이가 M이랑 같을 경우 출력
             for (int i = 0; i < M; i++) {
                 System.out.print(arr[i] + " ");
             }
             System.out.println();
             return;
         }
-        for (int i = 1; i <= N; i++) {
-            if (!isVisited[i]) {
-                arr[depth] = i;
-                isVisited[i] = true;
-                func(depth + 1);
-                isVisited[i] = false;
-            }
+
+        // i 는 at 부터 탐색하도록 한다.
+        for (int i = at; i <= N; i++) {
+            // 현재 깊이를 index 로 하여 해당 위치에 i 값을 담는다.
+            arr[depth] = i;
+
+            // 재귀 호출:
+            // 현재 i 값보다 다음 재귀에서 더 커야하므로
+            // i + 1 의 값을 넘겨주고, 깊이 또한 1 증가시켜 재귀 호출해준다.
+            dfs(i +1, depth + 1);
         }
     }
 }
