6da16ea555075e6ad01233544d9dec9efb1c244c
Dev-Beom <youngbeom0908@gmail.com>
2021-06-09 21:11:45 +0900

    [20210608] - 백준 7562

--- src/com/javaps/B7562/Main.java
+++ src/com/javaps/B7562/Main.java
@@ -14,6 +14,15 @@ class Location {
 }
 
 public class Main {
+    static int L;
+    static int[][] map;
+    static boolean[][] isVisited;
+
+    static int gx;
+    static int gy;
+    static int sx;
+    static int sy;
+
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st = new StringTokenizer(br.readLine());
@@ -21,27 +30,55 @@ public class Main {
         int TC = Integer.parseInt(st.nextToken());
 
         for (int idx = 0; idx < TC; idx++) {
-            int L = Integer.parseInt(br.readLine());    // 한변의 길이
-            int[][] map = new int[L][L];
-            int[][] isVisited = new int[L][L];
+            L = Integer.parseInt(br.readLine());    // 한변의 길이
+            map = new int[L][L];
+            isVisited = new boolean[L][L];
+
             for (int i = 0; i < L; i++) {
                 for (int j = 0; j < L; j++) {
                     map[i][j] = 0;
-                    isVisited[i][j] = 0;
+                    isVisited[i][j] = false;
                 }
             }
-            st = new StringTokenizer(br.readLine());
-            locations.add(new Location(Integer.parseInt(st.nextToken()), Integer.parseInt(st.nextToken())));
-            st = new StringTokenizer(br.readLine());
-            int gx = Integer.parseInt(st.nextToken());
-            int gy = Integer.parseInt(st.nextToken());
 
-            int xArr = {1, 0, -1, 0};
-            
+            st = new StringTokenizer(br.readLine());
+            gx = Integer.parseInt(st.nextToken());
+            gy = Integer.parseInt(st.nextToken());
 
+            st = new StringTokenizer(br.readLine());
+            sx = Integer.parseInt(st.nextToken());
+            sy = Integer.parseInt(st.nextToken());
+
+            int[] xArr = {1, 2, 2, 1, -1, -2, -2, -1};
+            int[] yArr = {-2, -1, 1, 2, 2, 1, -1, -2};
+
+            locations.clear();
+            locations.add(new Location(gx, gy));
+            isVisited[gx][gy] = true;
+
+            int rx;
+            int ry;
             while (!locations.isEmpty()) {
-
+                Location location = locations.poll();
+                int dx = location.x;
+                int dy = location.y;
+                if (dx == sx && dy == sy) {
+                    break;
+                }
+                for (int i = 0; i < 8; i++) {
+                    int nx = dx + xArr[i];
+                    int ny = dy + yArr[i];
+                    if (nx < 0 || nx >= L || ny < 0 || ny >= L) {
+                        continue;
+                    }
+                    if (!isVisited[nx][ny]) {
+                        locations.add(new Location(nx, ny));
+                        isVisited[nx][ny] = true;
+                        map[nx][ny] = map[dx][dy] + 1;
+                    }
+                }
             }
+            System.out.println(map[sx][sy]);
         }
     }
 }
