26147aa097637e211116dd57918c0419a8ef9a4e
Dev-Beom <youngbeom0908@gmail.com>
2021-06-22 23:27:55 +0900

    [20210622] - 프로그래머스 다리를 지나는 트럭

--- src/com/javaps/P다리를지나는트럭/Main_.java
+++ src/com/javaps/P다리를지나는트럭/Main_.java
@@ -1,59 +1,40 @@
 package com.javaps.P다리를지나는트럭;
 
-import java.io.IOException;
-import java.util.LinkedList;
 import java.util.Queue;
+import java.util.LinkedList;
 
-public class Main_ {
+class Solution {
+    public int solution(int bridge_length, int weight, int[] truck_weights) {
+        // 마지막 트럭을 제외한 모든 트럭이 다리를 통과하는 데 걸린 시간.
+        int answer = 0;
+        // 현재 다리의 트럭 무게 총합, 트럭들의 무게를 참조 할 변수.
+        int temp_weight = 0, idx = 0;
+        // 다리, 큐로 구현.
+        Queue<Integer> queue = new LinkedList<>();
 
-    public static void main(String[] args) throws IOException {
-
-        //  디버깅용
-        int bridge_length = 2;
-        int weight = 10;
-        int[] truck_weights = {7, 4, 5, 6};
-
-        System.out.println(new Solution().solution(bridge_length, weight, truck_weights));
-    }
-
-    public static class Solution {
-        public int solution(int bridge_length, int weight, int[] truck_weights) {
-            int time = 0;
-            int firstWaitTruck = 0;
-            int nowWeight = 0;
-            int tic = 0;
-            int firstWaitTruckCount = truck_weights.length;
-            int finishedTruckCount = 0;
-            int pollData = 0;
-
-            Queue<Integer> finishedTruck = new LinkedList<>();
-            Queue<Integer> inBridgeTruck = new LinkedList<>();
-            Queue<Integer> waitTruck = new LinkedList<>();
-
-            for (int i = 0; i < firstWaitTruckCount; i++) {
-                waitTruck.add(truck_weights[i]);
+        // 마지막 트럭을 제외한 모든 트럭을 통과시키기 위한 무한 반복.
+        while (true) {
+            // 마지막 트럭이 다리에 올라갔을 때, 벗어난다.
+            if (idx == truck_weights.length) break;
+            // 다리에 있는 트럭이 끝에 도달하면, 도달한 트럭의 무게를 현재 다리의 트럭 무게 총합에서 빼준다.
+            if (queue.size() == bridge_length) {
+                temp_weight -= queue.poll();
             }
-            while (!waitTruck.isEmpty() || !inBridgeTruck.isEmpty()) {
-                time++;
-                if (tic == bridge_length) {
-                    pollData = inBridgeTruck.poll();
-                    finishedTruck.add(pollData);
-                    nowWeight -= pollData;
-
-                    tic = 0;
-                    finishedTruckCount++;
-                }
-                if (!waitTruck.isEmpty()) {
-                    firstWaitTruck = waitTruck.peek();
-                    if (nowWeight + firstWaitTruck <= weight) {
-                        pollData = waitTruck.poll();
-                        inBridgeTruck.add(pollData);
-                        nowWeight += pollData;
-                    }
-                }
-                tic++;
+            // 현재 다리의 트럭 무게 총합 + 다리에 올라가야 하는 트럭의 무게 > 다리의 하중인 경우.
+            else if (temp_weight + truck_weights[idx] > weight) {
+                // 다리의 길이를 고려하기 위해 0인 값을 넣어 자리를 채우고, 1초 증가.
+                queue.offer(0);
+                answer++;
+                // 위를 제외하고는 다리에 트럭이 올라간다.
+            } else {
+                queue.offer(truck_weights[idx]);
+                temp_weight += truck_weights[idx];
+                answer++;
+                idx++;
             }
-            return time;
         }
+
+        // 마지막 트럭이 다리에 올라간 상태에서 다리의 길이를 더해주면, 모든 트럭이 통과하는데 걸린 시간.
+        return answer + bridge_length;
     }
 }
