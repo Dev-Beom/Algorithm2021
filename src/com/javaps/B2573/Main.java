1c7c5e92579c9a6bc54313921c2258f15cafaeb2
Dev-Beom <youngbeom0908@gmail.com>
2021-12-07 23:06:45 +0900

    [20211207] Graph, BFS, Implement - #38

--- src/com/javaps/B2573/Main.java
+++ src/com/javaps/B2573/Main.java
@@ -3,10 +3,7 @@ package com.javaps.B2573;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.Arrays;
-import java.util.LinkedList;
-import java.util.Queue;
-import java.util.StringTokenizer;
+import java.util.*;
 
 class Node {
     int x, y;
@@ -29,7 +26,7 @@ public class Main {
     static boolean[][] visited;
     static int year = 0;
 
-    static Queue<Node> icebergs = new LinkedList<>();
+    static ArrayList<Node> icebergs = new ArrayList<>();
 
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
@@ -43,7 +40,7 @@ public class Main {
             st = new StringTokenizer(br.readLine());
             for (int j = 0; j < M; j++) {
                 int value = Integer.parseInt(st.nextToken());
-                if (value != 0) icebergs.offer(new Node(j, i));
+                if (value != 0) icebergs.add(new Node(j, i));
                 map[i][j] = value;
             }
         }
